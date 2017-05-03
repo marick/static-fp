c6d376a5c8dcc9186f13fad2daf921dfacad9601
Brian Marick <marick@exampler.com>
2017-05-03 16:32:29 -0500

    Added a solution

--- elm/TypeAnnotations/ErrorsFixed.elm
+++ elm/TypeAnnotations/ErrorsFixed.elm
@@ -1,16 +1,30 @@
 module TypeAnnotations.ErrorsFixed exposing (..)
 
+-- See https://github.com/marick/static-fp/wiki/Type-annotations for commentary.
+
 f1 : Float -> Float
 f1 x =
   x / 3
 
+----------    
+    
 f2 : List a -> List a -> List a
 f2 one two =
   one ++ two
 
+-- or, just for fun
+
+f2a : List a -> List String -> List String
+f2a nonstrings strings =
+  List.map toString nonstrings ++ strings
+
+----------    
+    
 f3 : List a -> List a
 f3 list = List.reverse list
 
+----------    
+    
 f4: List a -> List a
 f4 list = List.reverse list
 
@@ -19,6 +33,7 @@ f4 list = List.reverse list
 f4a: List a -> List a
 f4a = List.reverse
 
+----------    
 
 f5: List a -> (a -> b) -> List b
 f5 = flip List.map    
