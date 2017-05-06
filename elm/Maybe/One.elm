004de6bfa16c3a9761c990d047ad96ae6b7b038e
Brian Marick <marick@exampler.com>
2017-05-06 16:55:38 -0500

    Maybe.

--- elm/Maybe/One.elm
+++ elm/Maybe/One.elm
@@ -1,22 +1,47 @@
+-- This is the first example of a module in a subdirectory. The module name
+-- requires that the file live in `<root>/Maybe/One.elm`.
+
 module Maybe.One exposing (..)
 
--- The name of this module signifies that the file lives in
--- `<root>/Maybe/One.elm`. You can import the file in three ways:
---
--- `import Maybe.One`
---    You have to refer to functions with a fully qualified name like
---    `Maybe.One.toBool`
---
--- `import Maybe.One exposing (..)`
---
---    You can refer to functions without any qualification: `toBool`.
---
--- `import Maybe.One as One`
---    This is a common convention for multi-component module names.
---    You can now use names like `One.toBool`
+{-
+                        * Importing a module * 
 
-toBool maybe = False
+You can import this module in three ways. The first two you've
+probably already seen in `Scratch.elm`. The last one is new.
 
-join maybe = Nothing
+`import Maybe.One`
+   You have to refer to functions with a fully qualified name like
+   `Maybe.One.toBool`
 
-toList maybe = []
+`import Maybe.One exposing (..)`
+
+   You can refer to functions without any qualification: `toBool`.
+
+`import Maybe.One as One`
+   This is a common convention for multi-component module names.
+   You can now use names like `One.toBool`
+-}
+
+{-
+The module named `Maybe` is automatically imported to all Elm
+programs, as are the two names `Just` and `Nothing`. Other names
+are qualified with the module name, like `Maybe.map`. 
+
+However, many of Elm's base types have community supported
+`<type>.Extra` modules that add useful functions. They're usually
+imported as follows. After that, you can use functions like
+`Maybe.values` without having to know which module it comes from.
+-}
+
+import Maybe.Extra as Maybe
+
+
+
+toBool maybe =
+  False
+
+join maybe =
+  Nothing
+
+toList maybe =
+  []
--- elm/Scratch.elm
+++ elm/Scratch.elm
@@ -1,22 +1,12 @@
 module Scratch exposing (..)
 
--- These are modules used in the book that are not automatically
--- imported.
-
+-- Elm automatically imports modules like `List` and `String`.
+-- Others need to be specifically imported. Here's an example:
 import Tuple
 
--- Many of Elm's base types have community supported `<type>.Extra`
--- modules that add useful functions. They're usually imported as
--- follows. After that, you can use functions like `Maybe.values`
--- without having to know which module it comes from.
---
--- `Maybe` itself is automatically imported to all Elm programs.
-import Maybe.Extra as Maybe
-
 -- An example function
-double : number -> number
-double x =
-  x + x
+length2 list1 list2 =
+  List.length list1 + List.length list2
 
 {- 
 Functions defined here are easy to use in the repl. Here are two ways:
@@ -25,36 +15,19 @@ Functions defined here are easy to use in the repl. Here are two ways:
    module functions.
 
 > import Scratch
-> Scratch.double 33
-66 : number
+> Scratch.length2 [1, 2] ["3", "4"]
+4 : Int
 
 2. You can ask for everything in the module to be exposed so the
    module name isn't needed.
 
 > import Scratch exposing (..)
-> double 33
-66 : number
+> length2 [1, 2] ["3", "4"]
+4 : Int
 
 If you change something in this file, repeat the `import` statement to
 make it available. NOTE: if you use the `exposing` form, and you want
 the changed functions to stay available without qualification by the
 module name, you have to repeat the whole line, including `exposing (..)`. 
-
--}
-
--- An example of a multi-line code snippet that would be inconvenient
--- to type in the repl.
-snippet =
-  Nothing
-    |> Maybe.map String.reverse 
-    |> Maybe.map (String.append "Dawn")
-
-{- 
-You can now see the result of the snippet by importing the module and
-typing `snippet` without any arguments.
-
-> import Scratch
-> Scratch.snippet
-Nothing : Maybe.Maybe String
 
 -}
