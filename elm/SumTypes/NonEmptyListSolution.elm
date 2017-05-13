74bc8b218da982c75d24937b544c5b326ea6dce2
Brian Marick <marick@exampler.com>
2017-05-13 16:54:20 -0500

    Added another exercise

--- elm/SumTypes/NonEmptyListSolution.elm
+++ elm/SumTypes/NonEmptyListSolution.elm
@@ -1,4 +1,4 @@
-module SumTypes.NonEmptyList exposing (..)
+module SumTypes.NonEmptyListSolution exposing (..)
 
 type NonEmptyList a =
   NonEmptyList a (List a)
@@ -17,5 +17,15 @@ head (NonEmptyList x _) = x
 tail : NonEmptyList a -> Maybe (NonEmptyList a)
 tail (NonEmptyList _ xs) =
   case xs of
+    [] -> Nothing
+    x :: xs -> Just (NonEmptyList x xs)
+
+toList : NonEmptyList a -> List a
+toList (NonEmptyList x xs) =
+  x :: xs
+  
+fromList : List a -> Maybe (NonEmptyList a)
+fromList list =
+  case list of
     [] -> Nothing
     x :: xs -> Just (NonEmptyList x xs)
