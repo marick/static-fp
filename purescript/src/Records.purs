b94a63d93a6b40a18f2bb7967774262680d52c8f
Brian Marick <marick@exampler.com>
2017-06-02 16:51:01 -0500

    Beginning of purescript repo

---
+++ purescript/src/Records.purs
@@ -0,0 +1,35 @@
+module Records where
+
+import Prelude
+
+newtype Animal = Animal
+  { id :: Int
+  , name :: String
+  , tags :: Array String
+  }
+
+bossie :: Animal
+bossie = { id: 13837, name: "bossie", tags: [] }
+
+jake :: Animal
+jake =
+  { id: 13838
+  , name: "jake"
+  , tags: ["stallion"]
+  }
+
+bossie2 :: Animal  
+bossie2 = bossie { name = "Bossy", tags = ["cow"] }
+
+friendlyName :: Animal -> String
+friendlyName {name, id} =
+  name <> " (" <> (show id) <> ")"
+
+
+
+-- comparison :: Animal -> String
+-- comparison ({name, id} as whole) = 
+--   "Which is better?   \n" ++
+--   name ++ " (" ++ (toString id) ++ ")\n" ++
+--   "\n...or:\n   " ++
+--   toString whole
