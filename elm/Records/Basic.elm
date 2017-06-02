80c2501ba2cf206f9f1e4f4e9f0172af672f0bab
Brian Marick <marick@exampler.com>
2017-06-02 16:50:41 -0500

    Basic record solutions

---
+++ elm/Records/Basic.elm
@@ -0,0 +1,111 @@
+module Records.Basic exposing (..)
+
+-- friendlyName : { record | name : String, id : Int } -> String
+-- friendlyName animal =
+--   animal.name ++ " (" ++ (toString animal.id) ++ ")"
+
+
+-- shorthand : Animal -> String
+-- shorthand animal =
+--   animal.name ++ " (" ++ (toString animal.id) ++ ")"
+
+hi : { record | name : String } -> String
+hi record =
+  "Hi " ++ record.name 
+
+type alias Animal =
+  { id : Int
+  , name : String
+  , tags : List String
+  }
+
+
+bossie : Animal  
+bossie =
+  { id = 13837
+  , name = "bossie"
+  , tags = ["heifer"]
+  }
+  
+
+type alias NamedIdThing whole =
+  { whole | id : Int, name : String }
+  
+shorthand : NamedIdThing record -> String
+shorthand record =
+  record.name ++ " (" ++ (toString record.id) ++ ")"
+
+
+ex1maker : String -> { name : String, id : Int }
+ex1maker name =
+  { name = name, id = 3 }
+
+ex1taker : { id : Int } -> Int
+ex1taker record =
+  record.id
+        
+
+    
+
+-- x : NamedIdThing { id : Float, name : Float }
+-- x = { id = 3 , name = "foo"}    
+    
+-- bossie =
+--   { id = 13837
+--   , name = "bossie"
+--   , tags = ["heifer"]
+--   }
+
+
+-- bossie : Animal
+-- bossie =
+--   { id = 13837
+--   , name = "bossie"
+--   , tags = []
+--   }
+
+-- jake : Animal
+-- jake =
+--   { id = 13838
+--   , name = "jake"
+--   , tags = ["stallion"]
+--   }
+
+-- bossie2 : Animal  
+-- bossie2 = { bossie | name = "Bossy", tags = ["cow"] }
+
+-- shorthand : Animal -> String
+-- shorthand {name, id} =
+--   name ++ " (" ++ (toString id) ++ ")"
+
+-- comparison : Animal -> String
+-- comparison ({name, id} as whole) = 
+--   "Which is better?   \n" ++
+--   name ++ " (" ++ (toString id) ++ ")\n" ++
+--   "\n...or:\n   " ++
+--   toString whole
+
+-- restrictiveName : { id : Int
+--                   , name : String
+--                   , tags : List String
+--                   } -> String
+-- restrictiveName {name, id} =
+--   name ++ " (" ++ (toString id) ++ ")"
+    
+-- shorthanddIdThing_broke : { id : Int
+--                              , name : String
+--                              } -> String
+-- shorthanddIdThing_broke {name, id} =
+--   name ++ " (" ++ (toString id) ++ ")"
+
+
+-- shorthanddIdThing : { whole | id : Int, name : String } -> String    
+-- shorthanddIdThing {name, id} =
+--   name ++ " (" ++ (toString id) ++ ")"
+    
+-- -- AuditedName whole = { whole | id : Int, name : String }
+
+
+-- setName : String -> Animal -> Animal
+-- setName newName animal =
+--   { animal | name = newName }
---
+++ elm/Records/BasicSolution.elm
@@ -0,0 +1,83 @@
+module Records.BasicSolution exposing (..)
+
+-- Problem 1
+
+maker : String -> { name : String, id : Int }
+maker name =
+  { name = name, id = 3 }
+
+taker : { id : Int } -> Int
+taker record =
+  record.id
+        
+takerVariant : { whole | id : Int } -> Int
+takerVariant record =
+  record.id
+
+-- makerVariant is impossible.
+
+
+{-
+   `makerVariant` is impossible. The executable code creates a
+   record with a `name` field, but `taker` allows only records
+   with exactly one field: `id`. 
+-}
+
+-- Problem 2
+
+{-
+   The unannotated `setName` had this type:
+
+      <function> : a -> { c | name : b } -> { c | name : a }
+
+   I didn't know that the { record | ... } notation could change not
+   only a field's value but also its *type*. Weird. 
+-}
+
+setName : part -> { whole | name : part } -> { whole | name : part }
+setName newValue record =
+  { record | name = newValue } 
+
+-- Problem 3
+
+type alias Lens3 whole part =
+  { get : { whole | name : part } -> part
+  , set : part -> { whole | name : part } -> { whole | name : part }
+  }
+  
+lens3 = { get = .name, set = setName }
+
+-- Problem 4
+
+type alias Lens4 whole part =
+  { get : whole -> part
+  , set : part -> whole -> whole
+  }
+
+lens4 : (whole -> part) -> (part -> whole -> whole) -> Lens4 whole part
+lens4 getter setter =
+  { get = getter, set = setter }
+
+-- Problem 5
+
+type alias Point = { x : Float, y : Float }
+
+myPoint = { x = 1.1, y = 3.3 }
+    
+type alias Lens whole part =
+  { get : whole -> part
+  , set : part -> whole -> whole
+  , update : (part -> part) -> whole -> whole
+  }
+
+  
+lens : (whole -> part) -> (part -> whole -> whole) -> Lens whole part
+lens getter setter =
+  let
+    updater f whole =
+      setter (f (getter whole)) whole
+  in
+    { get = getter, set = setter, update = updater }
+  
+pointX = lens .x (\newX whole -> { whole | x = newX })
+          
