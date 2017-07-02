d741e715d7a94d92d46bdc2b0be9fced15f04fff
Brian Marick <marick@exampler.com>
2017-07-02 18:15:39 -0500

    Added type annotations

--- elm/IVFinal/App/Animation.elm
+++ elm/IVFinal/App/Animation.elm
@@ -1,12 +1,23 @@
 module IVFinal.App.Animation exposing (..)
 
-import IVFinal.Types exposing (Msg(RunContinuation))
+import IVFinal.Types exposing (Msg(RunContinuation), Continuation)
 import Svg exposing (Svg)
 import Animation
 import Animation.Messenger
 import Ease
 import Time
+import IVFinal.Generic.EuclideanTypes exposing (Rectangle)
 import IVFinal.Generic.EuclideanRectangle as Rect
+import Color exposing (Color)
+
+-- Aliasing
+
+type alias Styling = Animation.Property
+type alias Timing = Animation.Interpolation  
+type alias Model = Animation.Messenger.State Msg
+type alias Step = Animation.Messenger.Step Msg
+
+type alias EasingFunction = Float -> Float      
 
 -- Tag attributes (including styles) that a particular shape
 -- never animates.
@@ -23,39 +34,54 @@ animatable shape (HasFixedPart attributes) animatedPart =
     (attributes ++ Animation.render animatedPart)
     []
 
-
+timing : EasingFunction -> Float -> Timing
 timing ease seconds = 
   Animation.easing
     { duration = Time.second * seconds
     , ease = ease
     }
       
+linear : Float -> Timing
 linear = timing Ease.linear
+
+accelerating : Float -> Timing
 accelerating = timing Ease.inQuad
 
-
+yFrom : Rectangle -> Styling
 yFrom = Rect.y >> Animation.y
+
+heightFrom : Rectangle -> Styling
 heightFrom = Rect.height >> px >> Animation.height
+
+request : Continuation -> Step             
 request = RunContinuation >> Animation.Messenger.send
-
--- Aliasing
-
-type alias Styling = Animation.Property
-type alias Timing = Animation.Interpolation  
-type alias Model = Animation.Messenger.State Msg
-type alias Step = Animation.Messenger.Step Msg
-
 
 
 -- re-exports
 
+height : Animation.Length -> Styling
 height = Animation.height
+
+fill : Color -> Styling          
 fill = Animation.fill
+
+px : Float -> Animation.Length       
 px = Animation.px
+
+y : Float -> Styling     
 y = Animation.y
 
+interrupt : List Step -> Model -> Model
 interrupt = Animation.interrupt
+
+loop : List Step -> Step
 loop = Animation.loop
+
+send : Msg -> Step       
 send = Animation.Messenger.send
+
+set : List Styling -> Step
 set = Animation.set
+
+toWith : Timing -> List Styling -> Step      
 toWith = Animation.toWith
