72441a01b62af4cfde75959cd83c7d1a3c890598
Brian Marick <marick@exampler.com>
2017-07-05 17:24:16 -0500

    ValidatedString

--- elm/IVFinal/Generic/ValidatedString.elm
+++ elm/IVFinal/Generic/ValidatedString.elm
@@ -1,10 +1,21 @@
-module IVFinal.Generic.ValidatedString exposing (..)
+module IVFinal.Generic.ValidatedString exposing
+  ( ValidatedString
+  , checked
+  )
+
+{-| A record that stores both a string and, maybe, the value it parses to.
+-}
 
 type alias ValidatedString a =
   { literal : String
   , value : Maybe a
   }
 
+{-| Construct a validated string with this. The name `checked` implies
+that the string has already been validated to produce the second argument
+(either a `Just` legitimate value or `Nothing`).
+-}
+  
 checked : String -> Maybe a -> ValidatedString a
 checked literal value =
   { literal = literal
