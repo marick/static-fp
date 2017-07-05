f87f1b5a956e5cafe9676d04e14e92de4e00d80c
Brian Marick <marick@exampler.com>
2017-07-05 08:39:19 -0500

    Try out documentation format

--- elm/IVFinal/Types.elm
+++ elm/IVFinal/Types.elm
@@ -1,16 +1,35 @@
-module IVFinal.Types exposing (..)
+module IVFinal.Types exposing
+  ( Model
+  , Msg
+  , Continuation
+  , FinishedForm
+  , ModelTransform
+  )
 
-import IVFinal.App.InputFields as Field
-import Animation.Messenger
-import IVFinal.Generic.Measures as Measure
+{-| Mainly the `Model` and `Msg` types, collected in this one
+model to avoid circular dependencies.
+
+# The Big Two
+@docs Model, Msg
+
+# Others
+@docs FinishedForm, ModelTransform, Continuation
+-}
+
 import IVFinal.Scenario exposing (Scenario)
 import IVFinal.Simulation.Types as Simulation
+import IVFinal.App.InputFields as Field
+import IVFinal.Generic.Measures as Measure
+import Animation.Messenger
 import Animation
+
 
 -- This needs to be here to avoid circular dependencies.
 -- Elsewhere, use App.Animation.Model
 type alias AnimationModel = Animation.Messenger.State Msg
 
+{-| A flat Model used by the whole app 
+-}  
 type alias Model =
   { scenario : Scenario
   , stage : Simulation.Stage
@@ -25,17 +44,18 @@ type alias Model =
   , hoseFluid : AnimationModel
   }
 
-  
-type Continuation = Continuation (Model -> Model)
-type alias ModelTransform = Model -> Model
-  
-
+{-| When all the `Field` values exist and are valid, they can
+be extracted to this structure, which can then be used for
+processing without worrying about `Maybe`. 
+-}
 type alias FinishedForm = 
   { dripRate : Measure.DropsPerSecond
   , hours : Measure.Hours
   , minutes : Measure.Minutes
   }
 
+{-| Single-level `Msg` type. 
+-}
 
 type Msg
   = ChangeDripRate String
@@ -55,3 +75,16 @@ type Msg
   -- Use this for tasks that produce no useful values, and
   -- when we don't even care whether is succeeded or not.
   | SideEffectTaskFinished
+
+
+{-| Parts of the app use continuation-passing style. The continuations
+are tagged with this type to make their purpose more clear.
+-}
+type Continuation = Continuation (Model -> Model)
+
+
+{-| Varous animation functions are most conveniently thought of as
+transformers for models, and are defined in point-free style. This
+alias labels them for easy recognition.
+-}
+type alias ModelTransform = Model -> Model
