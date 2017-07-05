0c9979f09305944b637eacb561789912874bc940
Brian Marick <marick@exampler.com>
2017-07-05 12:41:20 -0500

    cleanup: Html

--- elm/IVFinal/App/Html.elm
+++ elm/IVFinal/App/Html.elm
@@ -1,23 +1,50 @@
-module IVFinal.App.Html exposing (..)
+module IVFinal.App.Html exposing
+  ( soloButton
+  , textInput
+  )
 
+{-| Some "reusable components"
+-}
+
+import IVFinal.Generic.ValidatedString exposing (ValidatedString)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import IVFinal.App.InputFields as Field
-import IVFinal.Generic.ValidatedString exposing (ValidatedString)
 
+{-| A button that's expected to stand alone in its own `p`.
+
+    H.soloButton "Restart"
+      [ Event.onClick ResetSimulation ]
+-}
 soloButton : String -> List (Attribute msg) -> Html msg
 soloButton label attributes =
   Html.p []
     [ Html.button attributes 
         [strong [] [text label]]
     ]
-      
+
+
+{-| A small text input field that displays a `ValidatedString`
+
+    H.textInput desiredHours [ Event.onInput ChangeHours ]
+-}
 textInput : ValidatedString a  -> List (Html.Attribute msg) -> Html msg
-textInput validated eventHandlers = 
+textInput validated attributes = 
   input ([ type_ "text"
          , value validated.literal
          , size 6
-         , Field.border validated
-         ] ++ eventHandlers)
+         , visualValidation validated
+         ] ++ attributes)
   []
 
+
+-- Util
+
+visualValidation : ValidatedString a -> Html.Attribute msg
+visualValidation validated =
+  case validated.value of
+    Nothing ->
+      style [ ("border", "1px solid black")
+            , ("background-color", "pink")
+            ]
+    Just _ -> 
+      style [("border", "1px solid grey")]
