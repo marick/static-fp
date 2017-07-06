018107ea73f902495b70d16635cc88b6726e1d49
Brian Marick <marick@exampler.com>
2017-07-06 12:36:08 -0500

    MAIN.elm

--- elm/IVFinal/MAIN.elm
+++ elm/IVFinal/MAIN.elm
@@ -1,45 +1,53 @@
-module IVFinal.IV exposing (..)
+module IVFinal.MAIN exposing (..)
 
 import Html exposing (..)
 import Task
 
+import IVFinal.FormView as Form
+import IVFinal.Form.Types as Form
+import IVFinal.Form.InputFields as Field
 
 import IVFinal.ApparatusView as Apparatus
 import IVFinal.Apparatus.Droplet as Droplet
 import IVFinal.Apparatus.BagFluid as BagFluid
 import IVFinal.Apparatus.ChamberFluid as ChamberFluid
 import IVFinal.Apparatus.HoseFluid as HoseFluid
-import IVFinal.Form.InputFields as Field
+
 import IVFinal.Scenario as Scenario exposing (Scenario)
 import IVFinal.Simulation as Simulation
-import IVFinal.Generic.Measures as Measure
+import IVFinal.Simulation.Types exposing (..)
+import IVFinal.Types exposing (..)
+
 import IVFinal.App.Animation as Animation
 import IVFinal.App.Layout as Layout
-import IVFinal.FormView as FormView
-import IVFinal.Form.Types as Form
+import IVFinal.Generic.Measures as Measure
+
 import Dom
 
-import IVFinal.Types exposing (..)
-import IVFinal.Simulation.Types exposing (..)
 
 startingModel : Scenario -> Model
 startingModel scenario =
-  { scenario = scenario
-  , stage = FormFilling
+  let
+    bagStartingPercent =
+      Measure.proportion scenario.startingVolume scenario.containerVolume
+  in
+    { scenario = scenario
+    , stage = FormFilling
 
-  , desiredDripRate = Field.dripRate ""
-  , desiredMinutes = Field.minutes "0"
-  , desiredHours = Field.hours "0"
+    , desiredDripRate = Field.dripRate ""
+    , desiredMinutes = Field.minutes "0"
+    , desiredHours = Field.hours "0"
 
-
-  , droplet = Animation.style Droplet.initStyles
-  , bagFluid = Animation.style <| BagFluid.initStyles <| Measure.proportion scenario.startingVolume scenario.containerVolume
-  , chamberFluid = Animation.style ChamberFluid.initStyles
-  , hoseFluid = Animation.style HoseFluid.initStyles
+    , bagFluid = Animation.style <| BagFluid.initStyles bagStartingPercent
+    , chamberFluid = Animation.style ChamberFluid.initStyles
+    , hoseFluid = Animation.style HoseFluid.initStyles
+    , droplet = Animation.style Droplet.initStyles
   }
 
 init : (Model, Cmd Msg)
-init = ( startingModel Scenario.carboy, Cmd.none )
+init = ( startingModel Scenario.carboy
+       , Cmd.none
+       )
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -68,8 +76,7 @@ update msg model =
       , Cmd.none
       )
 
-    -- If the form is valid (expected) forward to simulation cases
-      
+    -- If the form is valid, forward to simulation cases
     DrippingRequested ->
       normallyForward (Form.dripRate model) StartDripping model
 
@@ -94,13 +101,14 @@ update msg model =
     Tick subMsg ->
       updateSimulations subMsg model
 
+        
     -- After the simulation
         
     ResetSimulation ->
       ( startingModel model.scenario
       , Task.attempt
           (always SideEffectTaskFinished)
-          (Dom.focus FormView.firstFocusId)
+          (Dom.focus Form.firstFocusId)
       )
 
     SideEffectTaskFinished ->
@@ -131,7 +139,7 @@ view : Model -> Html Msg
 view model =
   Layout.wrapper 
     [ Layout.canvas <| Apparatus.view model
-    , Layout.form <| FormView.view model
+    , Layout.form <| Form.view model
     ]
 
 subscriptions : Model -> Sub Msg
@@ -158,21 +166,27 @@ main =
 
 -- Util
 
-send : msg -> Cmd msg
-send msg =
+{-| Cause a message to be delivered. A roundabout way of calling what
+could just be another function. Done this way to show how a program can
+create its own special-purpose commands.
+-} 
+forward : msg -> Cmd msg
+forward msg =
   Task.perform identity (Task.succeed msg)
 
-maybeSend : (data -> msg) -> Maybe data -> Cmd msg
-maybeSend constructor maybe =
-  case maybe of
-    Nothing ->
-      Cmd.none
-    Just value ->
-      send (constructor value)
+{- Return an unchanged `Model` and a `Cmd` that sends a `Msg` to a
+later invocation of `update`.
 
+Takes a value wrapped in a `Maybe`. It should be "impossible" for
+that to be `Nothing`. If the impossible happens, nothing is done.
+
+Otherwise, the given function is used to make a `Cmd` that delivers a `Msg`. 
+-}
 normallyForward : Maybe value -> (value -> msg) -> model
                 -> (model, Cmd msg)        
-normallyForward checked destinationCase model =
-  ( model
-  , maybeSend destinationCase checked
-  )
+normallyForward maybe msgMaker model =
+  case maybe of
+    Nothing ->
+      (model, Cmd.none)
+    Just value ->
+      (model, forward (msgMaker value))
