6c2f79a2ba0f6746e895c74ad36bc282cc103d31
Brian Marick <marick@exampler.com>
2017-07-06 12:02:06 -0500

    EuclideanRectangle

--- elm/IVFinal/Generic/EuclideanRectangle.elm
+++ elm/IVFinal/Generic/EuclideanRectangle.elm
@@ -1,6 +1,23 @@
 module IVFinal.Generic.EuclideanRectangle exposing
-  ( ..
+  ( Rectangle
+  , x
+  , y
+  , width
+  , height
+
+  , fromOrigin
+  , lowerTo
+  , nudgeDown
+  , centeredBelow
+  , centeredAbove
   )
+
+{-| An abstract rectangle as Euclid would have described it: just a
+bunch of lengths and positions.
+
+Note that the `y` coordinate of a rectangle is the location of its
+top, and `height` enxtends downward.
+-}
 
 type alias Point = { x : Float, y : Float }
 type alias Size = { width : Float, height : Float }            
@@ -22,20 +39,24 @@ height rect = rect.size.height
 
 -- constructors
 
+{-| Create a rectangle at the upper left (coordinate 0, 0)
+-}
+
 fromOrigin : Float -> Float -> Rectangle  
 fromOrigin width height =
   { origin = Point 0 0, size = Size width height }
 
-translate : Point -> Rectangle-> Rectangle
-translate {x, y} { origin, size } =
-  { origin = Point (origin.x + x) (origin.y + y)
-  , size = Size size.width size.height
-  }
-
+{-| Increase the `y` value of a rectangle.
+-}
 nudgeDown : Float -> Rectangle -> Rectangle
 nudgeDown i =
   translate (Point 0 i)
 
+{-| Make the rectangle vertically shorter by decreasing the
+`height` and increasing the `y` value. The argument is a value
+between 0 and 1, where 1 means "no change", 0.5 means "half the size",
+and 1 would produce a degenerate rectangle with no height.
+-}
 lowerTo : Float -> Rectangle -> Rectangle  
 lowerTo percent { origin, size } =
   let
@@ -49,6 +70,12 @@ lowerTo percent { origin, size } =
     , size = Size size.width newHeight
     }
 
+{- Transform a rectangle to be directly below another (with its top
+edge having the same `y` value as the other's bottom edge), with both
+rectangles' centers on the same vertical line.
+
+The second parameter is the rectangle being "changed".
+-}
 centeredBelow : Rectangle -> Rectangle -> Rectangle
 centeredBelow above toBeBelow =
   let 
@@ -57,6 +84,12 @@ centeredBelow above toBeBelow =
   in
     translate (Point newX newY) toBeBelow
 
+{- Transform a rectangle to be directly above another (with its bottom
+edge having the same `y` value as the other's top edge), with both
+rectangles' centers on the same vertical line.
+
+The second parameter is the rectangle being "changed".
+-}
 centeredAbove : Rectangle -> Rectangle -> Rectangle
 centeredAbove below toBeAbove =
   let 
@@ -65,6 +98,9 @@ centeredAbove below toBeAbove =
   in
     translate (Point newX newY) toBeAbove
 
+
+-- Helpers
+      
 centerX : Rectangle -> Rectangle -> Float
 centerX existing toBeCentered = 
   let
@@ -72,3 +108,9 @@ centerX existing toBeCentered =
   in
     existingCenter - toBeCentered.size.width / 2
           
+translate : Point -> Rectangle-> Rectangle
+translate {x, y} { origin, size } =
+  { origin = Point (origin.x + x) (origin.y + y)
+  , size = Size size.width size.height
+  }
+
