cb202cb5836fad935d9be888ddd0c96d4bc7e68a
Brian Marick <marick@exampler.com>
2017-10-04 17:01:15 -0500

    More combiners

--- elm/Choose/Combine/Part.elm
+++ elm/Choose/Combine/Part.elm
@@ -2,7 +2,12 @@ module Choose.Combine.Part exposing (..)
 
 import Choose.Part as Part exposing (Part)
 import Choose.MaybePart as MaybePart exposing (MaybePart)
+import Choose.Combine.MaybePart as MaybePart
 import Choose.Case as Case exposing (Case)
 
-a = 3
-
+appendMaybePart : Part a b -> MaybePart b c -> MaybePart a c
+appendMaybePart a2b b2c =
+  MaybePart.append (MaybePart.fromPart a2b) b2c
+                      
+nextMaybePart : MaybePart b c -> Part a b -> MaybePart a c
+nextMaybePart = flip appendMaybePart
