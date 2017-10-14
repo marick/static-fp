5e323c7a4f676e5a5c5ce4752911ac73b71fc5ba
Brian Marick <marick@exampler.com>
2017-10-14 18:49:59 -0500

    Add tests to version 2

--- elm/tests/Lens/Try2/LensTest.elm
+++ elm/tests/Lens/Try2/LensTest.elm
@@ -2,6 +2,8 @@ module Lens.Try2.LensTest exposing (..)
 
 import Lens.Try2.Lens as Lens exposing (Lens)
 import Lens.Try2.Tuple2 as Tuple2
+import Lens.Try2.Tuple3 as Tuple3
+import Lens.Try2.Tuple4 as Tuple4
 
 import Test exposing (..)
 import TestBuilders exposing (..)
@@ -27,3 +29,38 @@ tuple2ObeysLaws =
   , Laws.lens "tuple2 - second"
       (Lens.ops Tuple2.second) (1, "OLD") "NEW" "overwritten"
   ]
+
+tuple3ObeysLaws =
+  concat
+  [ Laws.lens "tuple3 - first "
+      (Lens.ops Tuple3.first) ("OLD", 2, 3) "NEW" "overwritten"
+  , Laws.lens "tuple3 - second"
+      (Lens.ops Tuple3.second) (1, "OLD", 3) "NEW" "overwritten"
+  , Laws.lens "tuple3 - third"
+      (Lens.ops Tuple3.third) (1, 2, "third") "NEW" "overwritten"
+  ]
+
+tuple4ObeysLaws =
+  concat
+  [ Laws.lens "tuple4 - first "
+      (Lens.ops Tuple4.first) ("OLD", 2, 3, 4) "NEW" "overwritten"
+  , Laws.lens "tuple4 - second"
+      (Lens.ops Tuple4.second) (1, "OLD", 3, 4) "NEW" "overwritten"
+  , Laws.lens "tuple4 - third"
+      (Lens.ops Tuple4.third) (1, 2, "third", 4) "NEW" "overwritten"
+  , Laws.lens "tuple4 - fourth"
+      (Lens.ops Tuple4.fourth) (1, 2, 3, "fourth") "NEW" "overwritten"
+  ]
+
+
+lensPlusLensObeysLaws =
+  let
+    a2b = Lens.lens .b (\newB a -> {a | b = newB })
+    b2c = Lens.lens .c (\newC b -> {b | c = newC })
+    a2c = a2b |> Lens.andThen b2c 
+
+    a = { b = { c = "OLD" } }
+  in
+    Laws.lens "compose 2 lenses" (Lens.ops a2c) a "NEW" "overwritten"
+
+  
