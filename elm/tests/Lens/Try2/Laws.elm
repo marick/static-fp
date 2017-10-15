d048c835527a3c67ba26b9224b852f5b09351207
Brian Marick <marick@exampler.com>
2017-10-15 17:42:20 -0500

    Some work on weaklens

--- elm/tests/Lens/Try2/Laws.elm
+++ elm/tests/Lens/Try2/Laws.elm
@@ -14,16 +14,14 @@ setting_part_with_same_value_leaves_whole_unchanged {get, set} whole =
     "setting part with the same value leaves the whole unchanged"
 
 
-set_changes_only_the_given_part {get, set} whole overwritten part = 
+set_changes_only_the_given_part {set} whole overwritten part = 
   let
     a_direct_change =        whole |>                    set part
     change_that_overwrites = whole |> set overwritten |> set part
   in
-    equal change_that_overwrites a_direct_change
+    equal change_that_overwrites   a_direct_change
       "set changes only the given part (no counter, etc.)"
       
-
-
 
 lens comment unwrappedLens whole part overwritten = 
   describe comment
@@ -41,14 +39,27 @@ lens comment unwrappedLens whole part overwritten =
 -- Compare to set_part_can_be_gotten
 weaklens_overwrites {get, set} whole original new  =
   describe "when an element is present, set overwrites it"
-    [ equal (get          whole)      (Just original)  "show gets something"
+    [ equal  (get          whole)      (Just original)  "`get` gets original"
     , equal_ (get (set new whole))    (Just new)
+    ]
+
+weaklens_setting_what_gotten_changes_nothing {get, set} whole original =
+  describe "retrieving an element, then setting it"
+    [ equal  (get          whole)     (Just original)  "`get` gets original"
+    , equal_ (set original whole)     whole
     ]
 
 weaklens_does_not_create {get, set} whole new = 
   describe "when an element is missing, set does nothing"
-    [ equal (get          whole)       Nothing    "show gets nothing"
+    [ equal (get          whole)      Nothing     "show gets nothing"
     , equal (get (set new whole))     Nothing     "still gets nothing"
     , equal      (set new whole)      whole       "nothing else changed"
     ]
 
+
+-- how_weaklens_is_like_lens comment unwrappedLens whole oldPart newPart overwrittenPart = 
+--   describe comment
+--     [ Laws.weaklens_overwrites unwrappedLens whole oldPart newPart
+--     , Laws.weaklens_setting_what_gotten_changes_nothing unwrappedLens whole oldPart
+--     , Laws.set_changes_only_the_given_part unwrappedLens  whole overwritten newPart
+--     ]    
--- elm/tests/Lens/Try2/WeakLensTest.elm
+++ elm/tests/Lens/Try2/WeakLensTest.elm
@@ -31,8 +31,12 @@ arrayObeysLensLaws =
     fails = WeakLens.array 33
   in
     describe "Dict and lens laws "
-      [ Laws.weaklens_overwrites (unwrap found) whole "OLD" "NEW"
-      , Laws.weaklens_does_not_create (unwrap fails) whole "NEW"
+      [ -- This is the important difference
+        Laws.weaklens_does_not_create (unwrap fails) whole "NEW"
+
+      , Laws.weaklens_overwrites (unwrap found) whole "OLD" "NEW"
+      , Laws.weaklens_setting_what_gotten_changes_nothing (unwrap found) whole "OLD"
+      , Laws.set_changes_only_the_given_part (unwrap found) whole "Overwritten" "NEW"
       ]    
       
 
