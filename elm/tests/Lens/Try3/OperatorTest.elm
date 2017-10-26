ebf0634d8545b4cfa40244a42ee2aadbda711aac
Brian Marick <marick@exampler.com>
2017-10-26 15:02:24 -0500

    Remove unneeded parens

--- elm/tests/Lens/Try3/OperatorTest.elm
+++ elm/tests/Lens/Try3/OperatorTest.elm
@@ -13,6 +13,6 @@ import Lens.Try3.Array as Array
 -- These "tests" are only about trivial errors like typos in the type annotations.
 
 test_cc = Tuple2.first .... Tuple2.second
-test_cu = Tuple2.first ...^ (Dict.lens "key")
+test_cu = Tuple2.first ...^ Dict.lens "key"
 test_uc = Dict.lens "key" ^... Tuple2.first
-test_ii = Array.lens 0 ?..? (Array.lens 3)
+test_ii = Array.lens 0 ?..? Array.lens 3
