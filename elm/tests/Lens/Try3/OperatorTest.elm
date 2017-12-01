7f566f3e2dd1811ea0dbb074b3c86f974661abe2
Brian Marick <marick@exampler.com>
2017-12-01 12:49:30 -0600

    Change lens operators to new names

--- elm/tests/Lens/Try3/OperatorTest.elm
+++ elm/tests/Lens/Try3/OperatorTest.elm
@@ -9,10 +9,12 @@ import Lens.Try3.Compose.Operators exposing (..)
 import Lens.Try3.Tuple2 as Tuple2
 import Lens.Try3.Dict as Dict
 import Lens.Try3.Array as Array
+import Lens.Try3.Result as Result
 
 -- These "tests" are only about trivial errors like typos in the type annotations.
 
-test_cc = Tuple2.first .... Tuple2.second
-test_cu = Tuple2.first ...^ Dict.lens "key"
-test_uc = Dict.lens "key" ^... Tuple2.first
-test_hh = Array.lens 0 ?..? Array.lens 3
+test_cc = Tuple2.first ..>> Tuple2.second
+test_cu = Tuple2.first .^>> Dict.lens "key"
+test_uc = Dict.lens "key" ^.>> Tuple2.first
+test_hh = Array.lens 0 ??>> Array.lens 3
+test_oc = Result.ok |.>> Tuple2.second
