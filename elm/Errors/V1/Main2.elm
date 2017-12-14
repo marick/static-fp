fd264add64f8796167236fcc36f66352d92a2105
Brian Marick <marick@exampler.com>
2017-12-14 17:05:33 -0600

    Handle errors more better

---
+++ elm/Errors/V1/Main2.elm
@@ -0,0 +1,38 @@
+module Errors.V1.Main2 exposing (..)
+
+import Errors.V1.Basics exposing (..)
+import Errors.V1.Msg exposing (Msg(..))
+import Errors.V1.Model as Model exposing (Model)
+import Errors.V1.View as View 
+
+import Lens.Try3.Lens as Lens
+import Html
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  model
+    |> Lens.update Model.clickCount increment 
+    |> emphasizeWord msg 
+
+emphasizeWord : Msg -> Model -> (Model, Cmd Msg)
+emphasizeWord (EmphasizeWord person index) model =
+  case Lens.exists (Model.word person index) model of
+    False ->
+      noCmd model
+    True ->
+      model
+        |> Lens.set Model.beloved person
+        |> Lens.update (Model.wordCount person index) increment
+        |> noCmd
+
+noCmd : Model -> (Model, Cmd Msg)
+noCmd model = (model, Cmd.none)
+
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = Model.init
+    , view = View.view
+    , update = update
+    , subscriptions = always Sub.none
+    }
