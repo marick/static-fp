a5d3a3860d74e671522963fe6f910718e55a9af3
Brian Marick <marick@exampler.com>
2018-01-13 18:00:31 -0600

    Add `DecodeTest` as a typeclass example

---
+++ elm/tests/TypeClass/DecodeTest.elm
@@ -0,0 +1,49 @@
+module TypeClass.DecodeTest exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (Fuzzer)
+import TestBuilders exposing (..)
+
+import Json.Decode as Decode exposing (Decoder)
+
+
+decoderEquality : Fuzzer String -> Decoder a -> Decoder a -> String -> Test
+decoderEquality fuzzer left right comment =
+  let
+    results json =
+      ( Decode.decodeString left  json
+      , Decode.decodeString right json
+      )
+    runOne json =
+      results json |> uncurry Expect.equal
+  in
+    fuzz fuzzer comment runOne
+        
+intString : Fuzzer String
+intString =
+  Fuzz.frequency
+    [ (1 , Fuzz.map toString Fuzz.int)
+    , (1 , Fuzz.string)
+    ]
+
+identityLaw : Test
+identityLaw =
+  let
+    left = Decode.string 
+    right = Decode.map identity left
+  in
+    decoderEquality Fuzz.string left right       "identity"
+
+compositionLaw : Test
+compositionLaw = 
+  let
+    f = (*) 5
+    g = (+) 1
+        
+    left =  Decode.int |> Decode.map (f >>            g) 
+    right = Decode.int |> Decode.map  f |> Decode.map g  
+  in
+    decoderEquality intString left right        "composition"
+
+
