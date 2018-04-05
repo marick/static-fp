703e860a40100fc00b4c75f32111f6fcce393a04
Brian Marick <marick@exampler.com>
2018-04-05 14:45:42 -0500

    Tweaks for clarity.

--- purescript/blunder/src/Scratch.purs
+++ purescript/blunder/src/Scratch.purs
@@ -9,27 +9,26 @@ import Data.Array
 import Data.Tuple
 
 
---- exercise 1
+--- For part 1
 intList :: List Int
 intList = List.singleton 555
 
 tuple = Tuple 1.0 1
 
---- exercise 2
+--- For part 2
 intArray = singleton 3
 
--- exercises 3 and 4
+-- Parts 3 and 4
 data MyType wrapped = First wrapped | Second
 
 instance showMyType :: Show wrapped => Show (MyType wrapped) where
   show (First x) = "(First " <> show x <> ")"
   show Second  = "Second"
 
--- exercises 5 and 6
+-- Parts 5 and 6
 -- cases :: Partial => Int -> Int -> Int
 cases 0 0 = 10
 cases 0 1 = -1
 cases _ 3 = -3
 cases 8 _ = -8
-cases 8 8 = 3
 -- cases x y = x + y
